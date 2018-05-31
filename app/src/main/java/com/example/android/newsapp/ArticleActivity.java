e86ca2e287d462bda9a74062117c543ff4e77542
jlehocz <jozsef@lehocz.hu>
2018-05-31 21:03:23 +0200

    +main.xml

--- app/src/main/java/com/example/android/newsapp/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsapp/ArticleActivity.java
@@ -24,6 +24,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -150,5 +152,24 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
         // Loader reset, so we can clear out our existing data.
         Log.v("onLoadReset loader", "mAdapter.clear();");
         mAdapter.clear();
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
